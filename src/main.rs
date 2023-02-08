e391b3e894ac50988910127d1b4644e0492abb3d
Andrew Mary Huet de Barochez <phoqueeberlue@gmail.com>
2023-02-08 20:05:08 +0100

    Added command line argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    loop {} 
+use std::net::UdpSocket;
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// IP Address of the node to connect to
+    #[arg(short, long)]
+    ip: String,
+
+    /// Port of the node to connect to
+    #[arg(short, long)]
+    port: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+    println!("IP: {}", args.ip);
+    println!("Port: {}", args.port);
+    Ok(())
 }
